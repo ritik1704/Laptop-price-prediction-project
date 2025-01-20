b26ea387c6179e46c2be71601d9647674f8f29b6
Ritik1704 <Ritik1704@gmail.com>
2025-01-20 10:27:57 +0000

    Responsive changes

--- flaskapp.py
+++ flaskapp.py
@@ -55,4 +55,4 @@ def predict():
     return render_template('result.html', price=predicted_price)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
